67734f5d309367f74fbbd7fd12ea36730fd11e20
Dillon Beliveau <dillonbeliveau@gmail.com>
2018-10-23 20:07:56 -0400

    Nope, #define is the way to go

--- cpu.h
+++ cpu.h
@@ -2,14 +2,15 @@
 #include "mem.h"
 #include "util.h"
 
-const byte BRK = 0x0;
-const byte SEI = 0x78;
-const byte STA_Absolute = 0x8D;
-const byte TXS = 0x9A;
-const byte LDX_Immediate = 0xA2;
-const byte LDA_Immediate = 0xA9;
-const byte LDA_Absolute = 0xAD;
-const byte CLD = 0xD8;
+#define BRK 0x0
+#define SEI 0x78
+#define STA_Absolute 0x8D
+#define TXS 0x9A
+#define LDX_Immediate 0xA2
+#define LDA_Immediate 0xA9
+#define LDA_Absolute 0xAD
+#define CLD 0xD8
+
 
 byte read_byte_and_inc_pc(memory* mem);
 void cpu_step(memory* mem);
