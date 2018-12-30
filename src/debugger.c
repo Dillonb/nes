dbd185146752d7e0594ad6b9b78e38a518524fbf
Dillon Beliveau <dillonbeliveau@gmail.com>
2018-12-30 00:16:33 -0500

    Actually add the debugger.c/h files

---
+++ src/debugger.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+#include "debugger.h"
+#include "mem.h"
+#include "set.h"
+
+bool debug = false;
+
+address_tree* breakpoints = NULL;
+
+void set_breakpoint(uint16_t address) {
+    if (breakpoints == NULL) {
+        breakpoints = new_address_tree();
+    }
+    insert_to_address_tree(breakpoints, address);
+}
+
+bool is_breakpoint(uint16_t address) {
+    if (breakpoints == NULL) {
+        breakpoints = new_address_tree();
+    }
+    bool result = address_tree_contains(breakpoints, address);
+
+    return result;
+}
+
+bool breakpoint_on_interrupt = false;
+void set_breakpoint_on_interrupt() {
+    breakpoint_on_interrupt = true;
+}
+
+void set_debug() {
+    // Super hacky and non-portable way to not wait for enter after getchar().
+    system("stty -icanon");
+    debug = true;
+}
+
+typedef enum debugger_state_value_t {
+    RUNNING,
+    STOPPED,
+    STEPPING
+} debugger_state_value;
+
+debugger_state_value debugger_state = RUNNING;
+void process_debugger_command(char command) {
+    switch (command) {
+        case 's':
+            debugger_state = STEPPING;
+            return;
+        case 'c':
+            debugger_state = RUNNING;
+            return;
+        case 'q':
+            errx(EXIT_FAILURE, "User requested quit.");
+        default:
+            break;
+    }
+}
+
+void debugger_wait() {
+    debugger_state = STOPPED;
+    printf("s: step, c: continue, q: quit  >");
+    while (debugger_state == STOPPED) {
+        process_debugger_command(getchar());
+    }
+    printf("\n");
+}
+
+
+void debug_hook(debug_hook_type type, memory* mem) {
+    if (type == INTERRUPT && breakpoint_on_interrupt) {
+        debugger_wait();
+    }
+    else if (type == STEP) {
+        if (is_breakpoint(mem->pc) || debugger_state == STEPPING || debugger_state == STOPPED) {
+            debugger_wait();
+        }
+    }
+}
+
+bool debug_mode() {
+    return debug;
+}
---
+++ src/debugger.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "mem.h"
+
+typedef enum debug_hook_type_t {
+    STEP,
+    INTERRUPT
+} debug_hook_type;
+
+bool debug_mode();
+void debug_hook(debug_hook_type type, memory* mem);
+void set_debug();
+void set_breakpoint(uint16_t address);
+void set_breakpoint_on_interrupt();
