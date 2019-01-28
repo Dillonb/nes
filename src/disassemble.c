328fd86f1376ec835f306bf3cdc6c1bc7c565e20
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-01-28 18:35:49 -0500

    Beginnings of disassembler tool

---
+++ src/disassemble.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "rom.h"
+#include "mem.h"
+#include "debugger.h"
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("disassemble: disassemble PRG ROM of INES file\n");
+        printf("Usage: %s <rom.nes>\n", argv[0]);
+        return 2;
+    }
+    rom* r = read_rom(argv[1]);
+    memory mem = get_blank_memory(r);
+
+    char* disassembly = disassemble(&mem, mem.pc);
+
+    printf("%s", disassembly);
+
+    free(disassembly);
+}
