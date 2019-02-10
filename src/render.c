458363b4c19d4e4ba6f3fec493655e262c346dd2
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-02-10 10:23:46 -0500

    Screen scaling so I can actually see what's happening

--- src/render.c
+++ src/render.c
@@ -8,21 +8,19 @@
 
 #define SCREEN_WIDTH 256
 #define SCREEN_HEIGHT 240
+#define SCREEN_SCALE 4
 
 bool initialized = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-SDL_Texture* texture = NULL;
-byte* pixels = NULL;
 
 
 void initialize() {
     initialized = true;
-    pixels = malloc(4 * SCREEN_WIDTH * SCREEN_HEIGHT);
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         errx(EXIT_FAILURE, "SDL couldn't initialize! %s", SDL_GetError());
     }
-    window = SDL_CreateWindow("dgb nes", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("dgb nes", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE, SDL_WINDOW_SHOWN);
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -35,7 +33,7 @@ void render_screen(color screen[SCREEN_WIDTH][SCREEN_HEIGHT]) {
     if (!initialized) {
         initialize();
     }
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE);
 
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -48,27 +46,26 @@ void render_screen(color screen[SCREEN_WIDTH][SCREEN_HEIGHT]) {
     }
 
 
+    // TODO this is probably kinda slow
     for (int y = 0; y < 240; y++) {
         for (int x = 0; x < 256; x++) {
             color c = screen[x][y];
+            SDL_Rect rect;
+            rect.x = x * SCREEN_SCALE;
+            rect.y = y * SCREEN_SCALE;
+            rect.h = SCREEN_SCALE;
+            rect.w = SCREEN_SCALE;
 
-            int index = 4 * (x + y * SCREEN_WIDTH);
-
-            pixels[index] = c.b;
-            pixels[index + 1] = c.g;
-            pixels[index + 2] = c.r;
-            pixels[index + 3] = c.a;
+            SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+            SDL_RenderFillRect(renderer, &rect);
         }
     }
-    printf("Updating texture\n");
-    SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * 4);
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    printf("Updating renderer\n");
     SDL_RenderPresent(renderer);
 }
 
 
 // Not called from anywhere yet
 void cleanup() {
-    free(pixels);
+    //TODO cleanup SDL stuff
 }
