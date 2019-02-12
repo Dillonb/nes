236d9c5ee577d9750ed1fe9b84c4cd32f6a79017
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-02-11 23:33:17 -0500

    Start building a test around nestest.nes

---
+++ tests/test_nestest.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include "unity.h"
+#include <src/cpu.h>
+#include <src/opcode_names.h>
+#include <src/mem.h>
+
+#include <stdint.h>
+#include <src/debugger.h>
+
+typedef struct nestest_step_t {
+    uint16_t address;
+    byte a;
+    byte x;
+    byte y;
+    byte p;
+    byte sp;
+    byte ppu_x;
+    byte ppu_y;
+    int cycles;
+} nestest_step;
+
+rom* r;
+memory mem;
+
+void test_load_rom(void) {
+    if (r != NULL) {
+        free(r);
+    }
+
+    r = read_rom("nestest.nes");
+    mem = get_blank_memory(r);
+    mem.pc = 0xC000; // nestest: automated tests start at a different address
+}
+
+int num_steps = 8991;
+nestest_step steps[8991];
+void test_load_steps(void) {
+    FILE* fp = fopen("nestest.log", "rb");
+
+    for (int step = 0; step < num_steps; step++) {
+        nestest_step temp_step;
+
+        char buf[10];
+
+        fgets(buf, 5, fp);
+
+        temp_step.address = (uint16_t) strtol(buf, NULL, 16);
+
+        // Skip the raw byte values & disassembly
+        fseek(fp, 46, SEEK_CUR);
+
+        fgets(buf, 3, fp);
+        temp_step.a = (byte)strtol(buf, NULL, 16);
+
+        // Skip label
+        fseek(fp, 3, SEEK_CUR);
+
+        fgets(buf, 3, fp);
+        temp_step.x = (byte)strtol(buf, NULL, 16);
+
+        // Skip label
+        fseek(fp, 3, SEEK_CUR);
+
+        fgets(buf, 3, fp);
+        temp_step.y = (byte)strtol(buf, NULL, 16);
+
+        // Skip label
+        fseek(fp, 3, SEEK_CUR);
+
+        fgets(buf, 3, fp);
+        temp_step.p = (byte)strtol(buf, NULL, 16);
+
+        // Skip label
+        fseek(fp, 4, SEEK_CUR);
+
+        fgets(buf, 3, fp);
+        temp_step.sp = (byte)strtol(buf, NULL, 16);
+
+        // Skip label
+        fseek(fp, 5, SEEK_CUR);
+
+        fgets(buf, 4, fp);
+        temp_step.ppu_x = (byte)strtol(buf, NULL, 10);
+
+        // Skip label
+        fseek(fp, 1, SEEK_CUR);
+
+        fgets(buf, 4, fp);
+        temp_step.ppu_y = (byte)strtol(buf, NULL, 10);
+
+        // Skip label
+        fseek(fp, 5, SEEK_CUR);
+
+        fgets(buf, 9, fp);
+        temp_step.cycles = (byte)strtol(buf, NULL, 10);
+
+        steps[step] = temp_step;
+    }
+}
+
+void print_step_info(nestest_step stepdata) {
+    char* disassembly = disassemble(&mem, mem.pc);
+    printf("$%04X %-20s a: 0x%02X x: 0x%02X y: 0x%02X p: 0x%02X sp: 0x%02X \n", mem.pc, disassembly, mem.a, mem.x, mem.y, mem.p, mem.sp);
+    free(disassembly);
+    if (stepdata.address != mem.pc) {
+        printf("FAIL: We should be at address 0x%04X - we are at address 0x%04X\n", stepdata.address, mem.pc);
+    }
+}
+
+void test_run_rom(void) {
+    for (int step = 0; step < num_steps; step++) {
+        nestest_step stepdata = steps[step];
+        print_step_info(stepdata);
+        TEST_ASSERT_EQUAL_UINT8(stepdata.address, mem.pc);
+        cpu_step(&mem);
+    }
+}
+
+
+
+int main(void) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_load_rom);
+    RUN_TEST(test_load_steps);
+    RUN_TEST(test_run_rom);
+    return UNITY_END();
+}
