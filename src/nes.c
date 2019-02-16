893f704be0c21b5b65387900cfe611e182e0da89
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-02-16 15:33:09 -0500

    allow debugging without recompiling

--- src/nes.c
+++ src/nes.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "system.h"
 #include "cpu.h"
@@ -9,20 +10,31 @@
 #include "util.h"
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("Usage: %s <rom.nes>\n", argv[0]);
         return 2;
     }
 
-    //set_debug();
+    if (argc > 2) {
+        if (strcmp(argv[2], "debug") == 0) {
+            set_debug();
+            //set_breakpoint_on_interrupt();
+            set_breakpoints_for_rom(argv[1]);
+        }
+    }
+
+    if (argc > 3) {
+        if (strcmp(argv[3], "interrupt") == 0) {
+            set_breakpoint_on_interrupt();
+        }
+    }
+
 
     rom* r = read_rom(argv[1]);
 
     memory mem = get_blank_memory(r);
 
 
-    //set_breakpoint_on_interrupt();
-    //set_breakpoints_for_rom(argv[1]);
 
     while (true) {
         system_step(&mem);
