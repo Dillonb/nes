085ae9a18cff426854efeb3ce5d555cb86a20806
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-03-03 14:36:21 -0500

    Copy all 256 bytes in OAM DMA

--- src/cpu.c
+++ src/cpu.c
@@ -934,7 +934,7 @@ void trigger_nmi() {
 
 void trigger_oam_dma(memory* mem, uint16_t address) {
     byte oam_address = mem->ppu_mem.oam_address;
-    for (uint16_t i = 0; i < 0xFF; i++) {
+    for (uint16_t i = 0; i <= 0xFF; i++) {
         write_oam_byte(&mem->ppu_mem, read_byte(mem, address + i));
     }
 
