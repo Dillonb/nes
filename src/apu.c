0579f66333104efd4d0b4c75ce3b78498a7ae9a0
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-03-19 21:46:42 -0400

    Got two pulse channels working at a basic level

--- src/apu.c
+++ src/apu.c
@@ -8,11 +8,44 @@ byte read_apu_register(apu_memory* apu_mem, byte register_num) {
 
 }
 
+byte pulse_duty[4][8] = {
+        {0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0, 0, 0},
+        {1, 0, 0, 1, 1, 1, 1, 1}
+};
+
+void write_pulse_register(pulse_oscillator* pulse, int register_num, byte value, int pulsenum) {
+    switch (register_num) {
+        case 0: {
+            pulse->duty_value = (byte)((value & 0b11000000) >> 6);
+            break;
+        }
+        case 1:
+            break;
+        case 2: {
+            pulse->timer_register &= 0b11100000000;
+            pulse->timer_register |= value;
+            printf("PULSE %d: Timer Low written: Pulse timer is now %d\n", pulsenum, pulse->timer_register);
+            break;
+        }
+
+        case 3: {
+            pulse->timer_register &= 0b00011111111;
+            pulse->timer_register |= (value & 0b00000111) << 8;
+            printf("PULSE %d: Timer High written: Pulse timer is now %d\n", pulsenum, pulse->timer_register);
+            break;
+        }
+    }
+}
+
 void write_apu_register(apu_memory* apu_mem, int register_num, byte value) {
     if (register_num < 0x4) {
         // Pulse 1
+        write_pulse_register(&apu_mem->pulse1, register_num % 4, value, 1);
     } else if (register_num < 0x8) {
         // Pulse 2
+        write_pulse_register(&apu_mem->pulse2, register_num % 4, value, 2);
     }
     else if (register_num < 0xC) {
         // Triangle
@@ -37,16 +70,58 @@ void write_apu_register(apu_memory* apu_mem, int register_num, byte value) {
 apu_memory get_apu_mem() {
     apu_memory apu_mem;
     apu_mem.cycle = 0;
+    apu_mem.buffer_read_index = 0;
+    apu_mem.buffer_write_index = 0;
+
+    apu_mem.pulse1.timer_register = 0;
+    apu_mem.pulse2.timer_register = 0;
 
     return apu_mem;
+}
+
+float get_pulse_sample(pulse_oscillator* pulse) {
+    if (pulse->timer_register < 8) {
+        return 0.0f;
+    }
+    if (pulse_duty[pulse->duty_value][pulse->duty_step] == 1) {
+        return 1.0f;
+    }
+    else {
+        return -1.0f;
+    }
+}
+
+void step_pulse_timer(pulse_oscillator* pulse) {
+    if (pulse->timer_step == 0) {
+        pulse->timer_step = pulse->timer_register;
+        pulse->duty_step = (byte)((pulse->duty_step + 1) % 8);
+    }
+    else {
+        pulse->timer_step--;
+    }
 }
 
 void apu_step(apu_memory* apu_mem) {
     double last_cycle = apu_mem->cycle++;
     double this_cycle = apu_mem->cycle;
 
-    if ((int)(last_cycle / STEPS_PER_SAMPLE) != (int)(this_cycle / STEPS_PER_SAMPLE)) {
-        // TODO Generate a sample, place in ring buffer
+    if (apu_mem->buffer_write_index - apu_mem->buffer_read_index < 1) {
+        printf("Audio buffer underrun detected!\n");
+    }
+
+    if (apu_mem->cycle % 2 == 0) { // APU clock is half as fast as CPU
+        step_pulse_timer(&apu_mem->pulse1);
+        step_pulse_timer(&apu_mem->pulse2);
+    }
+
+    if ((int)(last_cycle / APU_STEPS_PER_SAMPLE) != (int)(this_cycle / APU_STEPS_PER_SAMPLE)) {
+        // TODO other oscs, and mix them
+        float pulse1_sample = get_pulse_sample(&apu_mem->pulse1);
+        float pulse2_sample = get_pulse_sample(&apu_mem->pulse2);
+
+        float sample = (0.5f * pulse1_sample) + (0.5f * pulse2_sample);
+        sample *= 0.10f;
+        apu_mem->buffer[(apu_mem->buffer_write_index++) % APU_RING_BUFFER_SIZE] = sample;
     }
 
 }
@@ -59,11 +134,16 @@ static int paCallback(const void *inputBuffer, void *outputBuffer,
                       void *userData) {
     apu_memory* apu_mem = (apu_memory*)userData;
     float* out = (float*)outputBuffer;
-    (void) inputBuffer; /* Prevent unused variable warning. */
+    (void) inputBuffer; // Prevent unused variable warning.
 
     for(uint32_t i = 0; i < framesPerBuffer; i++) {
         // Read sample from ring buffer. While the ring buffer is empty, push zeroes.
-        *out++ = 0;
+        if (apu_mem->buffer_read_index >= apu_mem->buffer_write_index) {
+            *out++ = 0.0f;
+        }
+        else {
+            *out++ = apu_mem->buffer[(apu_mem->buffer_read_index++) % APU_RING_BUFFER_SIZE];
+        }
     }
     return 0;
 }
--- src/apu.h
+++ src/apu.h
@@ -1,11 +1,28 @@
 #pragma once
 #include "util.h"
 
-static const double AUDIO_SAMPLE_RATE = 44100;
-static const double STEPS_PER_SAMPLE = CPU_FREQUENCY / AUDIO_SAMPLE_RATE;
+#define SAMPLE_RATE_DEF 44100
+
+static const double AUDIO_SAMPLE_RATE = SAMPLE_RATE_DEF;
+#define APU_RING_BUFFER_SIZE (SAMPLE_RATE_DEF * 2)
+static const double APU_STEPS_PER_SAMPLE = CPU_FREQUENCY / AUDIO_SAMPLE_RATE;
+
+typedef struct pulse_oscillator_t {
+    uint16_t timer_register; // 11 bit value. Written through register. Selects frequency.
+    uint16_t timer_step; // Progress through timer period. Updated internally.
+    byte duty_value; // 3 bit value. Written through register. Selects waveform.
+    byte duty_step; // Current position in the duty byte. Updated internally when timer hits 0.
+} pulse_oscillator;
 
 typedef struct apu_memory_t {
     long cycle;
+    float buffer[APU_RING_BUFFER_SIZE];
+    long buffer_write_index;
+    long buffer_read_index;
+
+    pulse_oscillator pulse1;
+    pulse_oscillator pulse2;
+
 } apu_memory;
 
 apu_memory get_apu_mem();
