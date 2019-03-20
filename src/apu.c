f8abda84710f57b3f6ac480414dc6dcac83a1d8b
Dillon Beliveau <dillonbeliveau@gmail.com>
2019-03-19 23:17:14 -0400

    Length counter working

--- src/apu.c
+++ src/apu.c
@@ -16,10 +16,17 @@ byte pulse_duty[4][8] = {
         {1, 0, 0, 1, 1, 1, 1, 1}
 };
 
+byte length_counter_table[] = {
+        10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26,
+        14, 12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16,
+        28, 32, 30 };
+
 void write_pulse_register(pulse_oscillator* pulse, int register_num, byte value, int pulsenum) {
     switch (register_num) {
         case 0: {
             pulse->duty_value = (byte)((value & 0b11000000) >> 6);
+            pulse->length_counter_halt = ((value & 0b00100000) >> 5) == 1;
+            pulse->constant_volume     = ((value & 0b00010000) >> 4) == 1;
             break;
         }
         case 1:
@@ -35,41 +42,11 @@ void write_pulse_register(pulse_oscillator* pulse, int register_num, byte value,
             pulse->timer_register &= 0b00011111111;
             pulse->timer_register |= (value & 0b00000111) << 8;
             printf("PULSE %d: Timer High written: Pulse timer is now %d\n", pulsenum, pulse->timer_register);
+
+            pulse->length_counter = length_counter_table[(value & 0b11111000) >> 3];
+            printf("PULSE %d: Loaded %d into length counter\n", pulsenum, pulse->length_counter);
             break;
         }
-    }
-}
-
-void write_apu_register(apu_memory* apu_mem, int register_num, byte value) {
-    if (register_num < 0x4) {
-        // Pulse 1
-        write_pulse_register(&apu_mem->pulse1, register_num % 4, value, 1);
-    } else if (register_num < 0x8) {
-        // Pulse 2
-        write_pulse_register(&apu_mem->pulse2, register_num % 4, value, 2);
-    }
-    else if (register_num < 0xC) {
-        // Triangle
-    }
-    else if (register_num < 0x10) {
-        // Noise
-    }
-    else if (register_num < 0x14) {
-        // DMC
-    }
-    else if (register_num == 0x15) {
-        // Channel enable and length counter status
-        //apu_mem->dmc.enable = (value >> 4) == 1;
-        //apu_mem->noise.enable = (value >> 3) == 1;
-        //apu_mem->triangle.enable = (value >> 2) == 1;
-        apu_mem->pulse2.enable = ((value >> 1) & 1) == 1;
-        apu_mem->pulse1.enable = (value & 1) == 1;
-    }
-    else if (register_num == 0x17) {
-        // Frame counter
-    }
-    else {
-        printf("WARNING: Unhandled APU register write to 0x%04X\n", (register_num + 0x4000));
     }
 }
 
@@ -85,11 +62,14 @@ apu_memory get_apu_mem() {
     apu_mem.pulse1.enable = 0;
     apu_mem.pulse2.enable = 0;
 
+    apu_mem.interrupt_inhibit = false;
+    apu_mem.frame_counter_mode = 0;
+
     return apu_mem;
 }
 
 float get_pulse_sample(pulse_oscillator* pulse) {
-    if (pulse->timer_register < 8 || pulse->enable == false) {
+    if (pulse->timer_register < 8 || pulse->enable == false || pulse->length_counter == 0) {
         return 0.0f;
     }
     if (pulse_duty[pulse->duty_value][pulse->duty_step] == 1) {
@@ -110,17 +90,54 @@ void step_pulse_timer(pulse_oscillator* pulse) {
     }
 }
 
+void pulse_dec_length_counter(pulse_oscillator* pulse) {
+    if (pulse->length_counter > 0 && !pulse->length_counter_halt) {
+        pulse->length_counter--;
+    }
+}
+
+void dec_length_counter(apu_memory* apu_mem) {
+    pulse_dec_length_counter(&apu_mem->pulse1);
+    pulse_dec_length_counter(&apu_mem->pulse2);
+}
+
+void step_frame_counter(apu_memory *apu_mem) {
+    switch (apu_mem->frame_counter_mode) {
+        case FC_4STEP:
+            apu_mem->frame_counter = (byte)((apu_mem->frame_counter + 1) % 4);
+            switch (apu_mem->frame_counter) {
+                case 1:
+                case 3:
+                    dec_length_counter(apu_mem);
+                    break;
+            }
+            break;
+        case FC_5STEP:
+            apu_mem->frame_counter = (byte)((apu_mem->frame_counter + 1) % 5);
+            switch (apu_mem->frame_counter) {
+                case 1:
+                case 3:
+                    dec_length_counter(apu_mem);
+            }
+            break;
+    }
+}
+
 void apu_step(apu_memory* apu_mem) {
     double last_cycle = apu_mem->cycle++;
     double this_cycle = apu_mem->cycle;
 
     if (apu_mem->buffer_write_index - apu_mem->buffer_read_index < 1) {
-        printf("Audio buffer underrun detected!\n");
+        //printf("Audio buffer underrun detected!\n");
     }
 
     if (apu_mem->cycle % 2 == 0) { // APU clock is half as fast as CPU
         step_pulse_timer(&apu_mem->pulse1);
         step_pulse_timer(&apu_mem->pulse2);
+    }
+
+    if ((int)(last_cycle / APU_STEPS_PER_FRAME_COUNTER_STEP) != (int)(this_cycle / APU_STEPS_PER_FRAME_COUNTER_STEP)) {
+        step_frame_counter(apu_mem);
     }
 
     if ((int)(last_cycle / APU_STEPS_PER_SAMPLE) != (int)(this_cycle / APU_STEPS_PER_SAMPLE)) {
@@ -147,8 +164,9 @@ static int paCallback(const void *inputBuffer, void *outputBuffer,
 
     for(uint32_t i = 0; i < framesPerBuffer; i++) {
         // Read sample from ring buffer. While the ring buffer is empty, push zeroes.
+        //while (apu_mem->buffer_read_index >= apu_mem->buffer_write_index);
         if (apu_mem->buffer_read_index >= apu_mem->buffer_write_index) {
-            *out++ = 0.0f;
+            *out++ = 0;
         }
         else {
             *out++ = apu_mem->buffer[(apu_mem->buffer_read_index++) % APU_RING_BUFFER_SIZE];
@@ -167,7 +185,7 @@ void apu_init(apu_memory* apu_mem) {
         errx(EXIT_FAILURE, "Unable to initialize PortAudio: %s", Pa_GetErrorText(err));
     }
 
-    err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, AUDIO_SAMPLE_RATE, 256, paCallback, apu_mem);
+    err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, AUDIO_SAMPLE_RATE, 32, paCallback, apu_mem);
 
 
     if (err != paNoError) {
@@ -180,4 +198,53 @@ void apu_init(apu_memory* apu_mem) {
         errx(EXIT_FAILURE, "Unable to start PortAudio stream: %s", Pa_GetErrorText(err));
     }
 
+}
+
+void write_apu_register(apu_memory* apu_mem, int register_num, byte value) {
+    if (register_num < 0x4) {
+        // Pulse 1
+        write_pulse_register(&apu_mem->pulse1, register_num % 4, value, 1);
+    } else if (register_num < 0x8) {
+        // Pulse 2
+        write_pulse_register(&apu_mem->pulse2, register_num % 4, value, 2);
+    }
+    else if (register_num < 0xC) {
+        // Triangle
+    }
+    else if (register_num < 0x10) {
+        // Noise
+    }
+    else if (register_num < 0x14) {
+        // DMC
+    }
+    else if (register_num == 0x15) {
+        // Channel enable and length counter status
+        //apu_mem->dmc.enable = (value >> 4) == 1;
+        //apu_mem->noise.enable = (value >> 3) == 1;
+        //apu_mem->triangle.enable = (value >> 2) == 1;
+        apu_mem->pulse2.enable = ((value >> 1) & 1) == 1;
+        if (!apu_mem->pulse2.enable) {
+            apu_mem->pulse2.length_counter = 0;
+        }
+
+        apu_mem->pulse1.enable = (value & 1) == 1;
+        if (!apu_mem->pulse1.enable) {
+            apu_mem->pulse1.length_counter = 0;
+        }
+    }
+    else if (register_num == 0x17) {
+        // Frame counter
+        apu_mem->frame_counter_mode = (value >> 7) & 1;
+        apu_mem->interrupt_inhibit = ((value >> 6) & 1) == 1;
+
+        if (apu_mem->frame_counter_mode == FC_5STEP) {
+            // clock envelope
+            // clock sweep
+            dec_length_counter(apu_mem);
+        }
+
+    }
+    else {
+        printf("WARNING: Unhandled APU register write to 0x%04X\n", (register_num + 0x4000));
+    }
 }
--- src/apu.h
+++ src/apu.h
@@ -4,26 +4,36 @@
 #define SAMPLE_RATE_DEF 44100
 
 static const double AUDIO_SAMPLE_RATE = SAMPLE_RATE_DEF;
-#define APU_RING_BUFFER_SIZE (SAMPLE_RATE_DEF * 2)
+#define APU_RING_BUFFER_SIZE 10000
 static const double APU_STEPS_PER_SAMPLE = CPU_FREQUENCY / AUDIO_SAMPLE_RATE;
+static const double APU_STEPS_PER_FRAME_COUNTER_STEP = CPU_FREQUENCY / 240.0;
+
+#define FC_4STEP 0
+#define FC_5STEP 1
 
 typedef struct pulse_oscillator_t {
     uint16_t timer_register; // 11 bit value. Written through register. Selects frequency.
     uint16_t timer_step; // Progress through timer period. Updated internally.
     byte duty_value; // 3 bit value. Written through register. Selects waveform.
     byte duty_step; // Current position in the duty byte. Updated internally when timer hits 0.
+    byte length_counter; // Set by register, from lookup table. Decremented internally.
     bool enable;
+    bool length_counter_halt;
+    bool constant_volume;
 } pulse_oscillator;
 
 typedef struct apu_memory_t {
     long cycle;
     float buffer[APU_RING_BUFFER_SIZE];
-    long buffer_write_index;
-    long buffer_read_index;
+    volatile long buffer_write_index;
+    volatile long buffer_read_index;
 
     pulse_oscillator pulse1;
     pulse_oscillator pulse2;
 
+    int frame_counter_mode;
+    bool interrupt_inhibit;
+    byte frame_counter;
 } apu_memory;
 
 apu_memory get_apu_mem();
